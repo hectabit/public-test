ef4c9e6823c323aa0cc630f642ca611e3cad4e4a
Keval Navadiya <kevalnavadiya39@gmail.com>
2022-06-28 00:54:48 +0530

    comment added

--- file1.js
+++ file1.js
@@ -1,3 +1,3 @@
-console.log("This change is done by Nirav1")
+console.log("This change is done by Nirav1"); //comment added by feature/first
 
-console.log("Second line added by Nirav")
+console.log("Second line added by Nirav");
